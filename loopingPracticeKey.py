a39e9662040f1ce40028579acbd337606554c1c5
Eric Darsow <ericdarsow@gmail.com>
2018-02-28 00:42:04 -0500

    initial setup after week 5

---
+++ loopingPracticeKey.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[15]:
+
+
+#challenge 1
+for z in range(101):
+    if z%2 == 0:
+        print(z, end=' ')
+
+
+# In[16]:
+
+
+#challenge 2
+k = list('KABOOM')
+for l in k:
+    for i in range(3):
+        print(l, end=' ')
+
+
+# In[22]:
+
+
+#challenge 3
+a = list("askaliceithinkshe'llknow")
+for c in range(0, len(a),2):
+    print(a[c], end=" ")
+
+
+# In[24]:
+
+
+print(range(3))
+r=range(3)
+for v in r:
+    print(v)
+
